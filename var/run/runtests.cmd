6e348cd4e6de8033b54c460dafcc01fd3c720884
Jeremy McGee <jeremy.mcgee@bassettdata.com>
2016-03-08 18:58:37 +0000

    See if by some odd fluke FireFox works where Chrome doesn't.

--- var/run/runtests.cmd
+++ var/run/runtests.cmd
@@ -1,3 +1,4 @@
-start /b chromedriver.exe
-krawlr -u=http://www.hiscoxcollection.com/default.aspx -w=Chrome
-taskkill /f /im chromedriver.exe
+rem start /b chromedriver.exe
+krawlr -u=http://www.hiscoxcollection.com/default.aspx 
+rem -w=Chrome
+rem taskkill /f /im chromedriver.exe
