872ff45c9c26a05e1854012a14d3345e6f85a6ce
Jeremy McGee <jeremy.mcgee@bassettdata.com>
2016-03-09 18:18:19 +0000

    First stub of XML, a bit of a proof-of-concept.

--- src/Krawlr.Core/Services/XmlWriter.cs
+++ src/Krawlr.Core/Services/XmlWriter.cs
@@ -5,25 +5,37 @@
     using System.IO;
     using Krawlr.Core.Extensions;
     using MZMemoize.Extensions;
-
+    using System.Xml.Linq;
     public class XmlWriter: Writer
     {
         private IConfiguration _configuration;
+        private XElement _rootElement;
+        private int _count;
 
         public XmlWriter(IConfiguration configuration, ILog log)
         {
             _configuration = configuration;
             Debug.WriteLine($"Writing to XML file {_configuration.XmlFile}...");
+            _rootElement = new XElement("test-results");
         }
 
         public override void Write(Response response)
         {
             Debug.WriteLine($" -- XML output to {_configuration.XmlFile}: {response.RelativeUrl}, {response.TimeTakenMs}, {response.Code}");
+            XElement testCaseElement = new XElement("test-case");
+            testCaseElement.SetAttributeValue("name", response.RelativeUrl);
+            testCaseElement.SetAttributeValue("executed", "True");
+            testCaseElement.SetAttributeValue("time", response.TimeTakenMs / 1000);
+            _rootElement.Add(testCaseElement);
+            _count++;
         }
 
         public override void Conclude()
         {
-            Debug.WriteLine($"Closing XML file {_configuration.XmlFile}");
+            _rootElement.SetAttributeValue("total", _count);
+
+            Debug.WriteLine(_rootElement.ToString());
+
             base.Conclude();
         }
     }
