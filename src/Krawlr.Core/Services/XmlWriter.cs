e5d3a2a5f4dbb9953076a4cbfb275d5a4cab5392
Jeremy McGee <jeremy.mcgee@bassettdata.com>
2016-03-21 15:09:31 +0000

    CLOUD-255: rework to use JUnit format for XML output.

--- src/Krawlr.Core/Services/XmlWriter.cs
+++ src/Krawlr.Core/Services/XmlWriter.cs
@@ -1,15 +1,17 @@
 ﻿namespace Krawlr.Core.Services
 {
     using System;
-    using System.Diagnostics;
     using System.IO;
     using System.Xml.Linq;
 
-    public class XmlWriter: Writer
+    /// <summary>
+    /// Writes output in JUnit XML format.
+    /// </summary>
+    public class XmlWriter : Writer
     {
-        private IConfiguration _configuration;
-        private ILog _log;
-        private XElement _resultsElement;
+        private readonly IConfiguration _configuration;
+        private readonly ILog _log;
+        private readonly XElement _testSuiteElement;
         private int _count;
         private decimal _totalTimeMillis;
 
@@ -18,87 +20,40 @@
             _configuration = configuration;
             _log = log;
 
-            //var environmentElement = new XElement("environment");
-            //environmentElement.SetAttributeValue("nunit-version", "Krawler");
-            //_rootElement.Add(environmentElement);
-
-            _resultsElement = new XElement("results");
+            _testSuiteElement = new XElement("testsuite");
         }
 
         public override void Write(Response response)
         {
             _count++;
 
-            XElement testCaseElement = new XElement("test-case");
+            XElement testCaseElement = new XElement("testcase");
             testCaseElement.SetAttributeValue("name", $"Test{_count}");
-            testCaseElement.SetAttributeValue("description", response.RelativeUrl);
-            testCaseElement.SetAttributeValue("executed", "True");
-            testCaseElement.SetAttributeValue("success", "True");
-            testCaseElement.SetAttributeValue("asserts", "0");
-            testCaseElement.SetAttributeValue("time", response.TimeTakenMs / 1000);
-            _resultsElement.Add(testCaseElement);
+            testCaseElement.SetAttributeValue("classname", response.RelativeUrl);
+            _testSuiteElement.Add(testCaseElement);
 
             _totalTimeMillis += response.TimeTakenMs;
         }
 
         public override void Conclude()
         {
-            var rootElement = GetTestResultsRootElement();
-            rootElement.Add(GetTestSuiteNode());
+            var rootElement = GetTestSuitesRootElement();
+            rootElement.Add(_testSuiteElement);
             WriteRootElementToFile(rootElement);
             base.Conclude();
         }
 
-        private XElement GetTestResultsRootElement()
+        private XElement GetTestSuitesRootElement()
         {
-            var element = new XElement("test-results");
+            var element = new XElement("testsuites");
             element.SetAttributeValue("name", "krawler-run");
             element.SetAttributeValue("date", $"{DateTime.Now:yyyy/MM/dd}");
             element.SetAttributeValue("time", $"{DateTime.Now:HH:mm:ss}");
-            element.SetAttributeValue("total", _count);
+            element.SetAttributeValue("tests", _count);
             element.SetAttributeValue("failures", "0");
             element.SetAttributeValue("not-run", "0");
+            element.SetAttributeValue("time", _totalTimeMillis);
             return element;
-        }
-
-        private XElement GetTestSuiteElement(string testSuiteName, XElement containedResultsElement)
-        {
-            var testSuiteElement = new XElement("test-suite", containedResultsElement);
-            testSuiteElement.SetAttributeValue("name", testSuiteName);
-            testSuiteElement.SetAttributeValue("success", "True");
-            testSuiteElement.SetAttributeValue("asserts", "0");
-            testSuiteElement.SetAttributeValue("time", _totalTimeMillis / 1000);
-            return testSuiteElement;
-        }
-
-        private XElement GetResultsElement(string testSuiteName, XElement containedResultsElement)
-        {
-            return new XElement(
-                "results", 
-                GetTestSuiteElement(testSuiteName, containedResultsElement)
-            );
-        }
-
-        private XElement GetTestSuiteNode()
-        {
-            return new XElement(
-                GetTestSuiteElement(
-                    "krawler-run", 
-                    GetResultsElement(
-                        "NUnit",
-                        GetResultsElement(
-                            "Tests",
-                            GetResultsElement(
-                                "Assembly",
-                                GetResultsElement(
-                                    "Fixture",
-                                    _resultsElement
-                                )
-                            )
-                        )
-                    )
-                )
-            );
         }
 
         private void WriteRootElementToFile(XElement rootElement)
@@ -107,7 +62,7 @@
             using (var writer = new StringWriter())
             {
                 xmlDocument.Save(writer);
-                
+
                 if (File.Exists(_configuration.XmlFile))
                 {
                     File.Delete(_configuration.XmlFile);
@@ -116,7 +71,7 @@
                 File.AppendAllText(_configuration.XmlFile, writer.ToString());
             }
 
-            _log.Info($"Written XML output in NUnit format to {Path.GetFullPath(_configuration.XmlFile)}.");
+            _log.Info($"Written XML output in JUnit format to {Path.GetFullPath(_configuration.XmlFile)}.");
         }
     }
 }
